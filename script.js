a513c41b5908b0a5948ec1bda3baeda18ebf76e8
Sukesh Reddy <117748776+Sukesh79@users.noreply.github.com>
2023-08-17 20:29:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let button = document.getElementById("button");
+button.addEventListener("click", () => {
+	let slide = document.getElementById("colorSelect");
+	let color = slide.value.toLowerCase();
+
+	let element = document.getElementById(color);
+	slide.removeChild(element);	
+})
